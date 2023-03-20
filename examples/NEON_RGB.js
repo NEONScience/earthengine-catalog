679b3bc47fc4329ffc13bf0158fcf2b3cdc60bd8
Bridget Hass <bhass@battelleecology.org>
2023-03-20 21:01:24 +0000

    remove print statement

--- examples/NEON_RGB.js
+++ examples/NEON_RGB.js
@@ -1,9 +1,7 @@
 var dataset = ee.ImageCollection("projects/neon-nonprod-earthengine/assets/DP3-30010-001")
   .filterDate('2021-01-01', '2021-12-31')
   .filterMetadata('NEON_SITE', 'equals', 'MCRA');
-  
-print(dataset)
-  
+ 
 var rgb = dataset.select(['B1', 'B2', 'B3']);
 var rgbVis = {min: 50, max: 200, gamma: 0.5};
 Map.setCenter(-122.154, 44.268, 14);
