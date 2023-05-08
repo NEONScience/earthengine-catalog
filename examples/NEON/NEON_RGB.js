fd94a7e88c8eeaf4961a37853edb37ba84f06ca5
Bridget Hass <bhass@battelleecology.org>
2023-05-08 21:32:42 +0000

    update to use 2021 ABBY data from the neon-prod-earthengine location

--- examples/NEON/NEON_RGB.js
+++ examples/NEON/NEON_RGB.js
@@ -1,8 +1,8 @@
-var dataset = ee.ImageCollection("projects/neon-nonprod-earthengine/assets/DP3-30010-001")
+var camera = ee.ImageCollection("projects/neon-prod-earthengine/assets/DP3-30010-001")
   .filterDate('2021-01-01', '2021-12-31')
-  .filterMetadata('NEON_SITE', 'equals', 'MCRA');
- 
-var rgb = dataset.select(['B1', 'B2', 'B3']);
-var rgbVis = {min: 50, max: 200, gamma: 0.5};
-Map.setCenter(-122.154, 44.268, 14);
-Map.addLayer(rgb, rgbVis, 'AOP RGB Camera Imagery');
+  .filterMetadata('NEON_SITE', 'equals', 'ABBY');
+  
+var rgb = camera.select(['B1', 'B2', 'B3']);
+var rgbVis = {min: 40, max: 200, gamma: 0.5};
+Map.setCenter(-122.32, 45.75, 12);
+Map.addLayer(rgb, rgbVis, 'ABBY 2021 RGB Camera Imagery');
