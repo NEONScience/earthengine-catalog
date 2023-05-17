17a9b6d72ab21e3211a7d3d3dd7588fffea6839c
Bridget Hass <101811216+bhass-neon@users.noreply.github.com>
2023-05-17 09:45:49 -0600

    Update NEON_DEM.js for new dual-band image format
    Re-ingested data with DSM and DTM as two layers in the same image, updated script for this new format.

--- examples/NEON/NEON_DEM.js
+++ examples/NEON/NEON_DEM.js
@@ -1,12 +1,17 @@
-var dsm = ee.ImageCollection('projects/neon-prod-earthengine/assets/DP3-30024-001')
+var dsm = ee.ImageCollection("projects/neon-prod-earthengine/assets/DP3-30024-001")
   .filterDate('2021-01-01', '2021-12-31')
   .filterMetadata('NEON_SITE', 'equals', 'SOAP')
-  .filterMetadata('PRODUCT_TYPE', 'equals', 'DSM');
+  .select('DSM')
+  .first();
 
-var dtm = ee.ImageCollection('projects/neon-prod-earthengine/assets/DP3-30024-001')
+var dtm = ee.ImageCollection("projects/neon-prod-earthengine/assets/DP3-30024-001")
   .filterDate('2021-01-01', '2021-12-31')
   .filterMetadata('NEON_SITE', 'equals', 'SOAP')
-  .filterMetadata('PRODUCT_TYPE', 'equals', 'DTM');
+  .select('DTM')
+  .first();
+  
+var dsm = dsm.updateMask(dsm.gte(0.0000));
+var dtm = dtm.updateMask(dtm.gte(0.0000));
   
 var demVis = {min: 600, max: 2300, gamma: 0.5};
 Map.setCenter(-119.25, 37.06, 12);
